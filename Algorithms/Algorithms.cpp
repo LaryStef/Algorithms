0ca41c0de4fda2183aecfff186790e99eb81ffda
Lary_Stef <timurkotov1999@gmail.com>
2024-05-27 00:29:40 +0300

    add nop

--- Algorithms/Algorithms.cpp
+++ Algorithms/Algorithms.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <vector>
+#include <windows.h>
 
 using namespace std;
 
@@ -23,64 +24,74 @@ public:
 	T* GetHead();
 };
 
-template <class T> Stack<T>::Stack() {			// 4
-	top = -1;				// 1
-	size = 1;				// 1
-	arr = new T[size];			// 2
+template <class T> Stack<T>::Stack() {
+	N_op = 0;
+	top = -1;				N_op += 1;
+	size = 1;				N_op += 1;
+	arr = new T[size];			N_op += 2;
 }
 
-template <class T> Stack<T>::~Stack() {			// 1
-	delete[] arr;			// 1
+template <class T> Stack<T>::~Stack() {
+	delete[] arr;
 }
 
-template <class T> void Stack<T>::Reallocate() {			// 7
-	T* temp = new T[size * 2];			// 2
-	memcpy(temp, arr, size*sizeof(T));			// 1
-	size *= 2;			// 2
-	delete[] arr;			// 1
-	arr = temp;			// 1
+template <class T> void Stack<T>::Reallocate() {
+	T* temp = new T[size * 2];			N_op += 2;
+	memcpy(temp, arr, size * sizeof(T));			N_op += 1;
+	size *= 2;			N_op += 2;
+	delete[] arr;			N_op += 1;
+	arr = temp;			N_op += 1;
 }
 
-template <class T> bool Stack<T>::IsEmpty() {			// 2
-	return top == -1;			// 2
+template <class T> bool Stack<T>::IsEmpty() {
+	N_op += 2;
+	return top == -1;
 }
 
-template <class T> int Stack<T>::GetLength() {			// 2
-	return top + 1;			// 2
+template <class T> int Stack<T>::GetLength() {
+	N_op += 2;
+	return top + 1;
 }
 
 template <class T> void Stack<T>::ShowAll() {
-	if (IsEmpty()) {			// 3
-		cout << "Stack is empty" << endl;			// 1
+	if (IsEmpty()) {
+		cout << "Stack is empty" << endl;
 	}
-	for (int i = GetLength(); i > 0; i--) {			// 4 + 1 + (n)(0)Σ(1 + 1 + 1)
-		cout << arr[i - 1] << " ";			// 1
+
+	for (int i = GetLength(); i > 0; i--) {
+		cout << arr[i - 1] << " ";
 	}
 }
 
-template <class T> void Stack<T>::Push(T value) {			// 6
-	if (GetLength() == size) {			// 4
-		Reallocate();			// 1
+template <class T> void Stack<T>::Push(T value) {
+	if (GetLength() == size) {
+		N_op += 2;
+		Reallocate();			N_op += 1;
 	}
-	arr[++top] = value;			// 2
+	arr[++top] = value;			N_op += 2;
 }
 
-template <class T> T Stack<T>::ShowHead() {			// 5
-	if (IsEmpty()) {			// 2
-		return NULL;			// 1
+template <class T> T Stack<T>::ShowHead() {
+	N_op += 1;
+	if (IsEmpty()) {
+		return NULL;			N_op += 1;
 	}
-	return arr[top];			// 2
+	N_op += 2;
+	return arr[top];
 }
 
-template <class T> T Stack<T>::Pop() {			// 5
-	if (IsEmpty()) {			// 2
-		return NULL;			// 1
+template <class T> T Stack<T>::Pop() {
+	N_op += 1;
+	if (IsEmpty()) {
+		return NULL;			N_op += 1;
 	}
-	return arr[top--];			// 2
+	N_op += 2;
+	return arr[top--];
 }
 
-template <class T> T* Stack<T>::GetHead() {			// 2
-	return &arr[top];			// 2
+template <class T> T* Stack<T>::GetHead() {
+	N_op += 2;
+	return &arr[top];
 }
 
 template <class T> class StackSort : public Stack<T> {
@@ -90,6 +101,7 @@ public:
 	using Stack<T>::ShowHead;
 	using Stack<T>::Push;
 	using Stack<T>::GetHead;
+	using Stack<T>::N_op;
 
 	T Get(int pos);
 	void Set(int pos, T num);
@@ -97,31 +109,36 @@ public:
 	T& operator[](int n);
 };
 
-template <class T> T StackSort<T>::Get(int pos) {			// 33 + 26n 
-	Stack<T> temp;			// 4
-	T target;			// 1
+template <class T> T StackSort<T>::Get(int pos) {
+	Stack<T> temp;			N_op += 1;
+	T target;			N_op += 1;
 
-	for (int i = GetLength() - 1; i > pos; i--) {		// 4 + 1 + (0)(n - 1)Σ(1 + 1 + 11)
-		temp.Push(Pop());			// 11
+	N_op += 4;
+	for (int i = GetLength() - 1; i > pos; i--) {
+		temp.Push(Pop());			N_op += 2;
 	}
-	target = ShowHead();			// 5
-	for (int i = temp.GetLength(); i > 0; i--) {			// 3 + 1 + (0)(n)Σ(1 + 1 + 11)
-		Push(temp.Pop());			// 11
+	target = ShowHead();
+	N_op += 3;
+	for (int i = temp.GetLength(); i > 0; i--) {
+		Push(temp.Pop());			N_op += 2;
 	}
 
-	return target;			// 1
+	N_op += 1;
+	return target;
 }
 
-template <class T> void StackSort<T>::Set(int pos, T num) {			// 26 + 26n
-	Stack<T> temp;			// 4
-	
-	for (int i = GetLength() - 1; i > pos; i--) {		// 4 + 1 + (0)(n - 1)Σ(1 + 1 + 11)
-		temp.Push(Pop());			// 11
+template <class T> void StackSort<T>::Set(int pos, T num) {
+	Stack<T> temp;			N_op += 1;
+
+	N_op += 4;
+	for (int i = GetLength() - 1; i > pos; i--) {
+		temp.Push(Pop());			N_op += 2;
 	}
 	Pop();
 	Push(num);
-	for (int i = temp.GetLength(); i > 0; i--) {			// 3 + 1 + (0)(n)Σ(1 + 1 + 11)
-		Push(temp.Pop());			// 11
+	N_op += 3;
+	for (int i = temp.GetLength(); i > 0; i--) {
+		Push(temp.Pop());			N_op += 2;
 	}
 }
 
@@ -129,73 +146,90 @@ template <class T> void StackSort<T>::Set(int pos, T num) {			// 26 + 26n
 //	return Get(n);
 //}
 
-template <class T> T& StackSort<T>::operator[](int n) {			// 31 + 26n
-	Stack<T> temp;			// 4
-	T* target;			// 1
+template <class T> T& StackSort<T>::operator[](int n) {
+	Stack<T> temp;			N_op += 1;
+	T* target;		 N_op += 1;
 
-	for (int i = GetLength() - 1; i > n; i--) {			// 4 + 1 + (0)(n - 1)Σ(1 + 1 + 11)
-		temp.Push(Pop());			// 11
+	N_op += 4;
+	for (int i = GetLength() - 1; i > n; i--) {
+		temp.Push(Pop()); N_op += 2;
 	}
-	target = GetHead();			// 2
-	for (int i = temp.GetLength(); i > 0; i--) {			// 3 + 1 + (0)(n)Σ(1 + 1 + 11)
-		Push(temp.Pop());			// 11
+	target = GetHead();
+	N_op += 3;
+	for (int i = temp.GetLength(); i > 0; i--) {
+		Push(temp.Pop());			N_op += 2;
 	}
 
-	return *target;			// 2
+	N_op += 2;
+	return *target;
 }
 
 template <class T> void StackSort<T>::Sort(int n) {
-	int m = 0;			// 2
+	int m = 0;			N_op += 2;
 
-	for (int i = 0; i < n; i++) {			// 2 + 1 + (0)(n)Σ(1 + 1 + 33 + 26n)
-		m = max(m, Get(i));			// 33 + 26n
+	N_op += 3;
+	for (int i = 0; i < n; i++) {
+		m = max(m, Get(i));			N_op += 4;
 	}
 
-	StackSort<int> countStack = StackSort<int>();
+	StackSort<int> countStack; N_op += 1;
 
-	for (int i = 0; i <= m; i++) {			// 2 + 1 + (0)(m)Σ(1 + 1 + 6)
-		countStack.Push(0);			// 6
+	N_op += 3;
+	for (int i = 0; i <= m; i++) {
+		countStack.Push(0);		N_op += 2;
 	}
 
-	for (int i = 0; i < n; i++) {			// 2 + 1 + (0)(n)Σ(1 + 1 + 130 + 104n)
-		countStack.Set(Get(i), countStack.Get(Get(i)) + 1);			// 131 + 104n
+	N_op += 3;
+	for (int i = 0; i < n; i++) {
+		countStack.Set(Get(i), countStack.Get(Get(i)) + 1);			N_op += 3;
 	}
 
-	for (int i = 1; i <= m; i++) {			// 2 + 1 + (0)(m)Σ(1 + 1 + 132 + 104n)
-		countStack.Set(i, countStack.Get(i) + countStack.Get(i - 1));			// 132 + 104n
-
+	N_op += 3;
+	for (int i = 1; i <= m; i++) {
+		countStack.Set(i, countStack.Get(i) + countStack.Get(i - 1));		N_op += 4;
 	}
 
-	StackSort<int> outputStack;			// 4
+	StackSort<int> outputStack;		N_op += 1;
 
-	for (int i = 0; i < n; i++) { outputStack.Push(0); }			// 2 + 1 + (0)(n)Σ(1 + 1 + 6)
+	N_op += 3;
+	for (int i = 0; i < n; i++) { outputStack.Push(0); N_op += 2; }
 
-	for (int i = n - 1; i >= 0; i--)			// 3 + 1 + (0)(n - 1)Σ(1 + 1 + 262 + 208n)
+	N_op += 4;
+	for (int i = n - 1; i >= 0; i--)
 	{
-		outputStack.Set(countStack.Get(Get(i)) - 1, Get(i));			// 131 + 104n
-		countStack.Set(Get(i), countStack.Get(Get(i)) - 1);			// 131 + 104n
+		outputStack.Set(countStack.Get(Get(i)) - 1, Get(i));			N_op += 1;
+		countStack.Set(Get(i), countStack.Get(Get(i)) - 1);		N_op += 1;
+		N_op += 2;
 	}
 
-	//*this = outputStack&;
-
-	for (int i = 0; i < n; i++) {			// 2 + 1 + (0)(n)Σ(1 + 1 + 36 + 26n)
-		Set(i, outputStack.Pop());			// 36 + 26n
+	N_op += 3;
+	for (int i = 0; i < n; i++) {
+		Set(i, outputStack.Pop());			N_op += 2;
 	}
+
+	N_op += countStack.N_op + outputStack.N_op;
 }
 
 int main()
 {
-	StackSort<int> stack;
+	unsigned long long int start, final;
 
-	for (int i = 1; i <= 10; i++) {
-		stack.Push(i);
+	for (int i = 1; i < 11; i++) {
+		StackSort<int> stack;
+
+		for (int j = 0; j < i * 10000; j++) {
+			stack.Push(rand() % 100);
+		}
+
+		start = GetTickCount64();
+		stack.Sort(i * 100);
+		final = GetTickCount64();
+
+		cout << "test: " << i << "\n";
+		cout << "number of elements: " << i * 100 << "\n";
+		cout << "sort time: " << final - start << "\n";
+		cout << "number of operations: " << stack.N_op << "\n";
+
+		cout << endl;
 	}
-	//stack.ShowAll();
-	//cout << endl;
-
-	//cout << stack[1] << endl;
-	stack[1] = 100;
-
-	stack.ShowAll();
-	cout << endl;
 }
